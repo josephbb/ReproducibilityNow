c722774f1e92cbe6b8bf1cf93fe44d4c5c9683b0
josephbb <jbakcoleman@gmail.com>
2026-07-22 07:55:18 -0400

    Iniitial commit with flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Barebones Nix shell with R and Python";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          R
+          python312
+        ];
+      };
+    };
+}
